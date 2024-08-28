725fac92f3228fb1ffa6dc401efd13b21da6e874
Ansh Patel <patelansh883@gmail.com>
2024-08-28 21:30:24 +0530

    fix: removed admin becuase of solid conflit

--- packages/admin/src/supabase/index.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const projectUrl = import.meta.env.VITE_PROJECT_URL
-const projectApiKey = import.meta.env.VITE_API_KEY
-
-export const supabase = createClient(projectUrl, projectApiKey)
