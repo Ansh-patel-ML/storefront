93197b122389e186379516dbde0b9b324dc5498f
Ansh Patel <patelansh883@gmail.com>
2024-08-29 06:05:46 +0530

    fix: Shadcn ui component css issue

--- packages/admin/tailwind.config.js
+++ packages/admin/tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@storefront/ui/**/*.{ts,tsx}' // Include your component library
   ],
   theme: {
     extend: {},
